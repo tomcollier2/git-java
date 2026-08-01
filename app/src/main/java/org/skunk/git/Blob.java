f4269b8bd87868f916138722fa7c962923233a27
Thomas Collier <colliertom9@gmail.com>
2026-08-01 21:43:22 +0100

    quick refactor

--- app/src/main/java/org/skunk/git/Blob.java
+++ app/src/main/java/org/skunk/git/Blob.java
@@ -7,21 +7,20 @@ import java.nio.file.Path;
 public class Blob extends GitObject {
 
     private Blob(byte[] data) {
-        this.data = data;
+        super(data);
     }
 
     public static Blob fromFile(Path file) throws IOException {
 
         byte[] contents = Files.readAllBytes(file);
 
-        String header = "blob " + contents.length + "\0";
+        return fromBytes(contents);
 
-        byte[] headerBytes = header.getBytes();
+    }
 
-        byte[] blob = new byte[headerBytes.length + contents.length];
+    public static Blob fromBytes(byte[] contents) {
 
-        System.arraycopy(headerBytes, 0, blob, 0, headerBytes.length);
-        System.arraycopy(contents, 0, blob, headerBytes.length, contents.length);
+        byte[] blob = buildObject(ObjectType.BLOB, contents);
 
         return new Blob(blob);
 
--- app/src/main/java/org/skunk/git/GitObject.java
+++ app/src/main/java/org/skunk/git/GitObject.java
@@ -2,10 +2,40 @@ package org.skunk.git;
 
 public class GitObject {
 
-    protected byte[] data;
+    public enum ObjectType {
+        BLOB,
+        TREE,
+        COMMIT,
+        TAG;
+
+        @Override
+        public String toString() {
+            return name().toLowerCase();
+        }
+    }
+
+    protected final byte[] data;
+
+    protected GitObject(byte[] data) {
+        this.data = data;
+    }
 
     public byte[] getData() {
         return data;
+    }
+
+    protected static byte[] buildObject(ObjectType type, byte[] contents) { 
+        
+        String header = type + " " + contents.length + "\0"; 
+    
+        byte[] headerBytes = header.getBytes(); 
+        byte[] object = new byte[headerBytes.length + contents.length]; 
+    
+        System.arraycopy(headerBytes, 0, object, 0, headerBytes.length); 
+        System.arraycopy(contents, 0, object, headerBytes.length, contents.length); 
+    
+        return object; 
+    
     }
     
 }
