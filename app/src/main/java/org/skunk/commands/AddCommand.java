782bea77c3b9d3b2534cc014e1a055c4c562742e
Thomas Collier <colliertom9@gmail.com>
2026-08-01 23:22:37 +0100

    added description

--- app/src/main/java/org/skunk/commands/AddCommand.java
+++ app/src/main/java/org/skunk/commands/AddCommand.java
@@ -2,10 +2,38 @@ package org.skunk.commands;
 
 import java.nio.file.Path;
 
+import org.skunk.git.Blob;
+import org.skunk.git.IndexEntry;
 import org.skunk.git.Repository;
 
 public class AddCommand {
 
+    /*
+    To mirror git add this command should stage a file by storing its contents
+    in the object database and recording an entry in the repository index.
+
+    The file is first converted into a blob object. A blob consists of a header
+    and the file contents in the following format:
+
+        "blob <size>\0<contents>"
+
+    The SHA-1 hash of the blob becomes the object's unique identifier. The blob
+    is compressed and written to the object database using the same layout as Git:
+
+        .skunk/objects/<first 2 hash characters>/<remaining 38 hash characters>
+
+    For example:
+
+        Hash:
+            e69de29bb2d1d6434b8b29ae775ad8c2e48c5391
+
+        Stored as:
+            .skunk/objects/e6/9de29bb2d1d6434b8b29ae775ad8c2e48c5391
+
+    Once the object has been written, an index entry is created containing the
+    blob's hash and the path to the file. The index acts as the staging area,
+    tracking which version of each file will be included in the next commit.
+    */
     public void execute(String[] args) {
 
         if (args.length != 2) {
@@ -19,7 +47,18 @@ public class AddCommand {
 
             Repository repository = Repository.open();
 
-            repository.add(Path.of(args[1]));
+            Path file = Path.of(args[1]);
+
+            Blob blob = Blob.fromFile(file);
+
+            String hash =
+                    repository
+                    .getObjectStore()
+                    .hash(blob, true);
+
+            repository
+                    .getIndex()
+                    .add(new IndexEntry(hash, file));
 
         }
         catch (Exception e) {
