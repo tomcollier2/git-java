515a0d31d1b1a3ae14fbe514571afcec4ba770db
Thomas Collier <colliertom9@gmail.com>
2026-08-02 11:56:12 +0100

    created Tree object

---
+++ app/src/main/java/org/skunk/git/Tree.java
@@ -0,0 +1,37 @@
+package org.skunk.git;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class Tree extends GitObject {
+
+    private Tree(byte[] contents) {
+        super(ObjectType.TREE, contents);
+    }
+
+    public static Tree fromIndex(List<IndexEntry> entries) {
+
+        StringBuilder builder = new StringBuilder();
+
+        for (IndexEntry entry : entries) {
+
+            builder.append("100644 ");
+
+            builder.append(entry.getPath());
+
+            builder.append(" ");
+
+            builder.append(entry.getHash());
+
+            builder.append("\n");
+
+        }
+
+        return new Tree(
+                builder.toString()
+                       .getBytes(StandardCharsets.UTF_8)
+        );
+
+    }
+    
+}
